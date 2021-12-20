a536e9f54b983662a89d60a994d6ff59dd388191
Mohammed Brighli <brmohamm@e2r3p8.1337.ma>
2021-12-20 18:20:04 +0100

    first

---
+++ pipex.c
@@ -0,0 +1,25 @@
+#include "pipex.h"
+
+int main(int argc, char *argv[], char **envp)
+{
+    int i;
+    char *path;
+    int if_access;
+
+    if_access = -1;
+    i = 0;
+    char *paths[] = {"/usr/local/bin/" ,"/usr/bin/","/bin/","/usr/sbin/","/sbin/","/usr/local/munki/"};
+
+    while (i != 6 &&  if_access == -1)
+    {
+        
+        path = ft_strjoin(paths[i],argv[1]);
+        if_access = access(path,F_OK);
+        if(if_access == -1)
+            free(path);
+        i++;
+    }
+    if (execve(path,&argv[1],envp) == -1)
+        perror("Could not execve");
+    return (0);
+}
---
+++ pipex.h
@@ -0,0 +1,10 @@
+# ifndef PIPEX_H
+#define PIPEX_H
+
+# include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
