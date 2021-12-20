7f448495f4d850e2186ca6216bc436649cd63e1a
Mohammed Brighli <brmohamm@e2r3p8.1337.ma>
2021-12-20 18:43:58 +0100

    3

--- pipex.c
+++ pipex.c
@@ -5,24 +5,21 @@ int main(int argc, char *argv[], char **envp)
     int i;
     char *path;
     int if_access;
-
+    (void)argc;
     if_access = -1;
     i = 0;
     char *paths[] = {"/usr/local/bin/" ,"/usr/bin/","/bin/","/usr/sbin/","/sbin/","/usr/local/munki/"};
-    if(argc == 2)
+    while (i != 6 &&  if_access == -1)
     {
-        while (i != 6 &&  if_access == -1)
-        {
         
-            path = ft_strjoin(paths[i],argv[1]);
-            if_access = access(path,F_OK);
-            if(if_access == -1)
-            free(path);
-            i++;
-        }
+        path = ft_strjoin(paths[i],argv[1]);
+        if_access = access(path,F_OK);
+        if(if_access == -1)
+        free(path);
+        i++;
+    }
         if (execve(path,&argv[1],envp) == -1)
             perror("Could not execve");
-    }
     
     return (0);
 }
