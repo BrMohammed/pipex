a2504a9955236dd7b3296e1ac47cd4464a83544d
Kali Live user <brmohamm>
2021-12-29 18:30:02 +0000

    bonus

--- pipex.c
+++ pipex.c
@@ -1,28 +1,23 @@
 #include "pipex.h"
 
-void creat_fille(int *fd,char *argv)
+int creat_fille(char *argv)
 {
     int output;
 
     output = open(argv,O_RDWR | O_CREAT,0777);
-    close(fd[1]);
-    dup2(fd[0],0);
     dup2(output,1);
     close(output);
-    close(fd[0]);
+    return(output);
 }
-
-void open_file(int *fd,char *argv)
+void open_file(char *argv)
 {
     int input;
 
     input =  open(argv,O_RDWR ,0777);
     if (input == -1)
         return;
-    close(fd[0]);
     dup2(input,0);
-    dup2(fd[1],1);
-    close(fd[1]);
+    close(input);
 }
 void path_finder(char **path,char **c)
 {
@@ -45,34 +40,38 @@ void path_finder(char **path,char **c)
 int main(int argc, char *argv[], char **envp)
 {
     char *path;
-    int fd[2];
+    int fd[2][2];
     (void)argc;
     char **c;
+    int id;
     //fd[0]read;
     //fd[1]write;
     //0 strin read
     //1 strout write
-    pipe(fd);
-    c = ft_split(argv[2],' ');
-    path_finder(&path,c);
-    int id = fork();
-    if(id == 0)
+    pipe(fd[0]);
+    pipe(fd[1]);
+    int t = 0; 
+    open_file(argv[1]);
+    while(t < 2)
     {
-        //child 
-        open_file(fd,argv[1]);
-        if (execve(path,&c[0],envp) == -1)
+        id = fork();
+        c = ft_split(argv[t+2],' ');
+        path_finder(&path,c);
+        if(id == 0)
+        {
+            close(fd[t][0]);
+            dup2(fd[t][1],1);
+            close(fd[t][1]);
+            if (execve(path,&c[0],envp) == -1)
             perror("Could not execve");
+        }
+        dup2(fd[t][0],0);
+        waitpid(id, NULL,0);
+        free(c);
+        t++;
     }
-    waitpid(id , NULL,0);
-    free(c);
-    c = ft_split(argv[3],' ');
-    path_finder(&path,c);
-    if(id != 0)
-    {
-        //parent
-        creat_fille(fd,argv[4]);
-        if (execve(path,&c[0],envp) == -1)
-            perror("Could not execve");
-    }
-    return (0);
+    t--;
+    creat_fille(argv[4]);
+    write(1,&fd[t][1],sizeof(fd[t][1]));
+    return (0);   
 }
