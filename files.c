28c9fc7bb2d19937d751397720de4f8343aea589
Mohammed Brighli <brmohamm@e2r9p4.1337.ma>
2022-01-31 17:23:50 +0100

    bonus19

--- files.c
+++ files.c
@@ -1,18 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   files.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brmohamm <brmohamm@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 17:19:21 by brmohamm          #+#    #+#             */
+/*   Updated: 2022/01/31 17:23:03 by brmohamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 
-void creat_fille(char *argv)
+void	creat_fille(char *argv)
 {
-    int output;
+	int	output;
 
-    output = open(argv,O_RDWR | O_CREAT,0777);
-    dup2(output,1);
-    close(output);
+	output = open(argv, O_RDWR | O_CREAT, 0777);
+	dup2(output, 1);
+	close(output);
 }
-int open_file(char *argv)
+
+int	open_file(char *argv)
 {
-    int input;
-    input =  open(argv,O_RDWR ,0777);
-    dup2(input,0);
-    close(input);
-    return(input);
-} 
+	int	input;
+
+	input = open(argv, O_RDWR, 0777);
+	dup2(input, 0);
+	close(input);
+	return (input);
+}
